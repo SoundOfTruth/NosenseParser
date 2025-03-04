ff45c58c5f23e8f60edd193cffad0a213f3ad3db
SoundOfTruth <catastrophist@mail.ru>
2025-03-04 15:09:12 +0300

    bdu parser fix

--- utils/cwe.py
+++ utils/cwe.py
@@ -3,13 +3,23 @@ import pandas
 
 
 def get_cwe(df: pandas.DataFrame, bdu: str) -> list:
-    value = df.loc[df['Идентификатор'] == bdu]
-    cwe = value['Тип ошибки CWE'].values[0]
-    return cwe.split(', ')
+    cwe_set = set()
+    try:
+        bdu_list = bdu.split()
+        for _bdu in bdu_list:
+            value = df.loc[df['Идентификатор'] == _bdu]
+            cwe = value['Тип ошибки CWE'].values[0]
+            if not type(cwe) is float:
+                cwe_str = ''.join(cwe.split(', '))
+                cwe_set.add(cwe_str)
+        return list(cwe_set)
+    except Exception as ex:
+        print(ex)
+        print(cwe_set)
 
 
 if __name__ == '__main__':
     df = pandas.read_excel('utils/vullist.xlsx')
-    cwe = get_cwe(df, 'BDU:2023-08343')
+    cwe = get_cwe(df, 'BDU:2015-00227')
     print(type(cwe))
     print(cwe)
--- utils/dbu.py
+++ utils/dbu.py
@@ -1,3 +1,4 @@
+import json
 from bs4 import BeautifulSoup
 
 
@@ -11,14 +12,20 @@ def get_bdu():
     bdu_table = soup.find('table', class_='vulnerabilitiesTbl')
     table_rows = bdu_table.find_all('tr')
     for row in table_rows:
-        row_bdu = row.find(class_='bdu')
-        if row_bdu:
-            row_desc = row.find(class_='desc')
-            bdu_dict[row_bdu.text] = {
+        bdu_data = row.find(class_='bdu')
+        bdu_td = ' '.join(str(bdu_data).split('<br/>'))
+        row_desc = row.find(class_='desc')
+        if bdu_td != 'None':
+            data = BeautifulSoup(bdu_td, 'html.parser')
+            bdu_str = data.text
+            bdu_dict[bdu_str] = {
                 'desc': row_desc.text
             }
     return bdu_dict
 
 
 if __name__ == '__main__':
-    print(get_bdu())
+    dbu = get_bdu()
+    with open('debug.json', 'w', encoding='utf-8') as data_fp:
+        json.dump(dbu, data_fp, indent=2, ensure_ascii=False)
+
